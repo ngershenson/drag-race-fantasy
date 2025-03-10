c4c223a30972e9ed92417ab8ebd95f373900ed01
Noah Gershenson <158068575+ngershenson@users.noreply.github.com>
2025-03-10 16:52:33 -0400

    extract initial data to constant file

---
+++ CONSTANTS.ts
@@ -0,0 +1,104 @@
+const ORIGINAL_TEAMS = [
+    {
+      id: 1,
+      name: "Feeling the Fantasy",
+      owner: "Chloe",
+      totalPoints: 0,
+      picks: {
+        1: [],
+      },
+    },
+    {
+      id: 2,
+      name: "Ru's Ruse",
+      owner: "Noah",
+      totalPoints: 0,
+      picks: {
+        1: [],
+      },
+    },
+    {
+      id: 3,
+      name: "Silly Sluts",
+      owner: "Julia",
+      totalPoints: 0,
+      picks: {
+        1: [],
+      },
+    },
+  ]
+
+const ORIGINAL_QUEENS = [
+    {
+      id: 1,
+      name: "Acacia Forgot",
+      points: 0,
+    },
+    {
+      id: 2,
+      name: "Arrietty",
+      points: 0,
+    },
+    {
+      id: 3,
+      name: "Crystal Envy",
+      points: 0,
+    },
+    {
+      id: 4,
+      name: "Hormona Lisa",
+      points: 0,
+    },
+    {
+      id: 5,
+      name: "Jewels Sparkles",
+      points: 0,
+    },
+    {
+      id: 6,
+      name: "Joella",
+      points: 0,
+    },
+    {
+      id: 7,
+      name: "Kori King",
+      points: 0,
+    },
+    {
+      id: 8,
+      name: "Lana Ja'Rae",
+      points: 0,
+    },
+    {
+      id: 9,
+      name: "Lexi Love",
+      points: 0,
+    },
+    {
+      id: 10,
+      name: "Lucky Starzzz",
+      points: 0,
+    },
+    {
+      id: 11,
+      name: "Lydia B Kollins",
+      points: 0,
+    },
+    {
+      id: 12,
+      name: "Onya Nurve",
+      points: 0,
+    },
+    {
+      id: 13,
+      name: "Sam Star",
+      points: 0,
+    },
+    {
+      id: 14,
+      name: "Suzie Toot",
+      points: 0,
+    },
+  ]
+
+  export {ORIGINAL_QUEENS, ORIGINAL_TEAMS}
--- src/App.tsx
+++ src/App.tsx
@@ -1,119 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dashboard from "./components/Dashboard";
 import LeagueControls from "./components/LeagueControls";
 import QueenAssignmentBoard from "./components/QueenAssignmentBoard";
 import PointsTable from "./components/PointsTable";
 import { LeagueMetadata, Team, Queen } from "./types";
+import {ORIGINAL_QUEENS, ORIGINAL_TEAMS} from "../CONSTANTS.ts"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 const App = () => {
-  const [teams, setTeams] = useState<Team[]>([
-    {
-      id: 1,
-      name: "Feeling the Fantasy",
-      owner: "Chloe",
-      totalPoints: 0,
-      picks: {
-        1: [],
-      },
-    },
-    {
-      id: 2,
-      name: "Ru's Ruse",
-      owner: "Noah",
-      totalPoints: 0,
-      picks: {
-        1: [],
-      },
-    },
-    {
-      id: 3,
-      name: "Silly Sluts",
-      owner: "Julia",
-      totalPoints: 0,
-      picks: {
-        1: [],
-      },
-    },
-  ]);
-  const [queens, setQueens] = useState<Queen[]>([
-    {
-      id: 1,
-      name: "Acacia Forgot",
-      points: 0,
-    },
-    {
-      id: 2,
-      name: "Arrietty",
-      points: 0,
-    },
-    {
-      id: 3,
-      name: "Crystal Envy",
-      points: 0,
-    },
-    {
-      id: 4,
-      name: "Hormona Lisa",
-      points: 0,
-    },
-    {
-      id: 5,
-      name: "Jewels Sparkles",
-      points: 0,
-    },
-    {
-      id: 6,
-      name: "Joella",
-      points: 0,
-    },
-    {
-      id: 7,
-      name: "Kori King",
-      points: 0,
-    },
-    {
-      id: 8,
-      name: "Lana Ja'Rae",
-      points: 0,
-    },
-    {
-      id: 9,
-      name: "Lexi Love",
-      points: 0,
-    },
-    {
-      id: 10,
-      name: "Lucky Starzzz",
-      points: 0,
-    },
-    {
-      id: 11,
-      name: "Lydia B Kollins",
-      points: 0,
-    },
-    {
-      id: 12,
-      name: "Onya Nurve",
-      points: 0,
-    },
-    {
-      id: 13,
-      name: "Sam Star",
-      points: 0,
-    },
-    {
-      id: 14,
-      name: "Suzie Toot",
-      points: 0,
-    },
-  ]);
+  const [teams, setTeams] = useState<Team[]>([]);
+  const [queens, setQueens] = useState<Queen[]>([]);
   const [isSelectionPhase, setIsSelectionPhase] = useState(true);
   const [metadata, setMetadata] = useState<LeagueMetadata>({
     currentWeek: 1,
     eliminatedQueens: [],
   });
+
+  useEffect(() => {
+    setTeams(ORIGINAL_TEAMS)
+    setQueens(ORIGINAL_QUEENS)
+  }, [])
 
   const endSelectionPhase = () => {
     setIsSelectionPhase(false);
